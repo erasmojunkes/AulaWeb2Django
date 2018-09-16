c2a43db4a25ac9408402e8a6bd8587800cbd73e0
erasmojunkes <37718439+erasmojunkes@users.noreply.github.com>
2018-09-16 20:56:45 -0300

    Models e Transações

--- cliente/admin.py
+++ cliente/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Caregoria
 from .models import Transacao
+from .models import Evento
+from .models import Funcionario
 
 # Register your models here.
+
 admin.site.register(Caregoria)
 admin.site.register(Transacao)
+admin.site.register(Evento)
+admin.site.register(Funcionario)
--- cliente/models.py
+++ cliente/models.py
@@ -9,3 +9,22 @@ class Transacao(models.Model):
     descricao = models.CharField(max_length = 300)
     valor = models.DecimalField(max_digits= 7, decimal_places= 2)
     catergoria = models.ForeignKey(Caregoria, on_delete = models.CASCADE)
+
+class Evento(models.Model):
+	data  = models.DateTimeField()
+	tema = models.CharField(max_length = 100)
+	descricao  = models.CharField(max_length = 500)
+	local = models.CharField(max_length = 120)
+	Caregoria = models.ForeignKey(Caregoria, on_delete=models.CASCADE)
+	class Meta:
+		verbose_name_plural = 'Evento'
+	def __str__(self):
+		return self.descricao
+
+class Funcionario(models.Model):
+	nome = models.CharField(max_length = 250)
+	dataNasc = models.DateTimeField()
+	cargo = models.CharField(max_length = 150)
+	salario = models.DecimalField(max_digits = 7,decimal_places = 2)
+	class Meta:
+		verbose_name_plural = 'Funcionário'
